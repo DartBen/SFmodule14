4b36b79e80165a758eb9e040f171ece31005c746
Dmitry <ryabchikk@rambler.ru>
2022-12-10 18:44:38 +0300

    14.0

---
+++ SF Module 14/14.0.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+        //string a = "А";
+        //char ch = a[0];
+
+        List<string> list = new List<string>();
+
+        foreach (string s in people)
+        {
+            //if (s[0] == ch)
+            if (s.ToUpper().StartsWith("А"))
+            { list.Add(s); }
+            continue;
+        }
+
+        if (list.Count > 0)
+        {
+            list.Sort();
+        }
+
+        foreach (string s in list)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+}
---
+++ SF Module 14/14.0.8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _14._0._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+                {
+                   1,
+                   "Сергей ",
+                   "Андрей ",
+                   300,
+                };
+
+            var result = from p in objects
+                         where (p is string)
+                         orderby p
+                         select p;
+
+            foreach (var r in result)
+                Console.WriteLine(r);
+
+
+            Console.WriteLine();
+            foreach (var r in objects.Where(p => p is string).OrderBy(p => p))
+                Console.WriteLine(r);
+        }
+    }
+}
